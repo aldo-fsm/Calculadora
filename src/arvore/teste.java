5e550aee4c44d507f1f85794351db87722c862e7
Aryell Dias de Menezes <aryelld@gmail.com>
2016-10-20 23:18:08 -0300

    Update teste.java

--- src/arvore/teste.java
+++ src/arvore/teste.java
@@ -6,7 +6,7 @@ public class teste {
 		/*a.armazeneExpressao("(((0.00000000000000000000000000000000000009+0.0000000002)-b)/0.00000000000000001)");
 		a.expressaoArmazenada();
 		*/
-		a.armazeneExpressao("(((6 / 3) + 2) * 3)");
+		a.armazeneExpressao("     ( ( ( ( 3 / 2 ) + ( 1 * 2 ) ) * 10 ) - ( a + b ) )");
 		System.out.println(a.calcularExpressao());
 		
 		
