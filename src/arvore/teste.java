32686782865e49dbc7370c48c17be91c5e3b16c3
Aryell Dias de Menezes <aryelld@gmail.com>
2016-10-21 19:04:01 -0300

    Update teste.java

--- src/arvore/teste.java
+++ src/arvore/teste.java
@@ -3,12 +3,9 @@ package arvore;
 public class teste {
 	public static void main(String[] args) {
 		ArvoreExpressoes a = new ArvoreExpressoes();
-		/*a.armazeneExpressao("(((0.00000000000000000000000000000000000009+0.0000000002)-b)/0.00000000000000001)");
-		a.expressaoArmazenada();
-		*/
-		a.armazeneExpressao("     ( ( ( ( 3 / 2 ) + ( 1 * 2 ) ) * 10 ) - ( a + b ) )");
+		a.armazeneExpressao("( 3 / 0.5 )");
+		a.acrescentaNaExpressao("- ( a / b )");
+		System.out.println(a.expressaoEmOrdem());
 		System.out.println(a.calcularExpressao());
-		
-		
 	}
 }
