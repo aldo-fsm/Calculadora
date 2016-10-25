5415992cbb08a412561f93bc7a0d0c458361a937
aldo <aldo.fsmonteiro@gmail.com>
2016-10-25 13:43:46 -0300

    ajustes

--- src/estruturasDados/arvore/Node.java
+++ src/estruturasDados/arvore/Node.java
@@ -1,30 +1,33 @@
 package estruturasDados.arvore;
 
 public class Node<T> {
-	
+
 	private Node<T> rightNode;
+	private Node<T> leftNode;
+	private T elemento;
+
 	public Node<T> getRightNode() {
 		return rightNode;
 	}
+
 	public void setRightNode(Node<T> rightNode) {
 		this.rightNode = rightNode;
 	}
 
-	private Node<T> leftNode;
 	public Node<T> getLeftNode() {
 		return leftNode;
 	}
+
 	public void setLeftNode(Node<T> leftNode) {
 		this.leftNode = leftNode;
 	}
-	
-	
-	private T elemento;
+
 	public T getElemento() {
 		return elemento;
 	}
+
 	public void setElemento(T elemento) {
 		this.elemento = elemento;
 	}
-	
+
 }
--- src/testes/TesteArvoreExpressoes.java
+++ src/testes/TesteArvoreExpressoes.java
@@ -5,7 +5,7 @@ import estruturasDados.arvore.ArvoreExpressoes;
 public class TesteArvoreExpressoes {
 	public static void main(String[] args) {
 		ArvoreExpressoes a = new ArvoreExpressoes();
-		a.armazeneExpressao("(2^3)");
+		a.armazenarExpressao("(2^3)");
 		System.out.println(a.expressaoEmOrdem());
 		System.out.println(a.calcularExpressao());
 	}
