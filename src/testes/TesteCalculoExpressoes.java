d031262fe473ce876e7b7653944b91c8c70691c8
aldo <aldo.fsmonteiro@gmail.com>
2016-10-26 17:33:39 -0300

    outra classe de teste

---
+++ src/testes/TesteCalculoExpressoes.java
@@ -0,0 +1,33 @@
+package testes;
+
+import java.util.Scanner;
+
+import estruturasDados.arvore.ArvoreExpressoes;
+
+public class TesteCalculoExpressoes {
+	public static void main(String[] args) {
+
+		ArvoreExpressoes calculadora = new ArvoreExpressoes();
+
+		System.out.println("\nTeste - Calculadora de Expressões");
+		System.out.println("digite exit para sair\n\n");
+		String input = "";
+		Scanner s = new Scanner(System.in);
+
+		while (true) {
+
+			System.out.printf(">>> ");
+			input = s.nextLine();
+			if (input.equals("exit")) {
+				break;
+			}
+			try {
+				calculadora.armazeneExpressao(input);
+				System.out.println(calculadora.expressaoEmOrdem() + " = " + calculadora.calcularExpressao());
+			} catch (RuntimeException e) {
+				e.printStackTrace(System.out);
+			}
+		}
+
+	}
+}
