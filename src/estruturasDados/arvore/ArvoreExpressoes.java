f7e3dfad0ef47c80b41eb2eff0a928d2546f8da8
aldo <aldo.fsmonteiro@gmail.com>
2016-10-27 17:55:44 -0300

    update - raiz

--- src/estruturasDados/arvore/ArvoreExpressoes.java
+++ src/estruturasDados/arvore/ArvoreExpressoes.java
@@ -1,4 +1,5 @@
 package estruturasDados.arvore;
+
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
@@ -10,61 +11,66 @@ import estruturasDados.pilha.pilhaArray;
 public class ArvoreExpressoes {
 	private Node<String> root;
 	private MathContext mathContext = new MathContext(100, RoundingMode.HALF_EVEN);
-	
-	public void acrescentaNaExpressao(String expressao){
+
+	public void acrescentaNaExpressao(String expressao) {
 		tradutorDeExpressoes("(" + expressaoEmOrdem() + expressao + ")");
 	}
-	
+
 	public void armazeneExpressao(String expressao) {
 		int i = 0;
 		boolean precisaAesquerda = false;
 		boolean precisaAdireita = false;
-		while(expressao.charAt(i) == ' ') i++;
-		if(expressao.charAt(i) != '(') precisaAesquerda = true;
-		i = expressao.length()-1;
-		while(expressao.charAt(i) == ' ') i--;
-		if(expressao.charAt(i) != '(') precisaAesquerda = true;
-		if(expressao.charAt(0) != '(' && expressao.charAt(expressao.length()-1) != ')' || precisaAdireita&&precisaAesquerda){
+		while (expressao.charAt(i) == ' ')
+			i++;
+		if (expressao.charAt(i) != '(')
+			precisaAesquerda = true;
+		i = expressao.length() - 1;
+		while (expressao.charAt(i) == ' ')
+			i--;
+		if (expressao.charAt(i) != '(')
+			precisaAesquerda = true;
+		if (expressao.charAt(0) != '(' && expressao.charAt(expressao.length() - 1) != ')'
+				|| precisaAdireita && precisaAesquerda) {
 			tradutorDeExpressoes("(" + expressao + ")");
-		}else if(expressao.charAt(0) != '(' && expressao.charAt(expressao.length()-1) == ')'|| precisaAesquerda){
+		} else if (expressao.charAt(0) != '(' && expressao.charAt(expressao.length() - 1) == ')' || precisaAesquerda) {
 			tradutorDeExpressoes("(" + expressao);
-		}else if(expressao.charAt(0) == '(' && expressao.charAt(expressao.length()-1) != ')'|| precisaAesquerda){
+		} else if (expressao.charAt(0) == '(' && expressao.charAt(expressao.length() - 1) != ')' || precisaAesquerda) {
 			tradutorDeExpressoes(expressao + ")");
-		}else{
+		} else {
 			tradutorDeExpressoes(expressao);
 		}
 	}
-		
+
 	public String expressaoEmOrdem() {
 		return auxExpressaoEmOrdem(root);
 	}
-	
+
 	public void informarVariaveis() {
 		ListaArray<String> variaveis = new ListaArray<String>();
 		ListaArray<String> valoresCorrespondentes = new ListaArray<String>();
 		auxInformaVariaveis(root, variaveis, valoresCorrespondentes);
 	}
-	
+
 	public BigDecimal calcularExpressao() {
-		ListaArray<String> variaveis = new ListaArray<String>(); 
+		ListaArray<String> variaveis = new ListaArray<String>();
 		ListaArray<BigDecimal> valoresCorrespondentes = new ListaArray<BigDecimal>();
-		return auxCalcularExpressao(root,variaveis,valoresCorrespondentes);
+		return auxCalcularExpressao(root, variaveis, valoresCorrespondentes);
 	}
-	
-	private void tradutorDeExpressoes(String expressao){
+
+	private void tradutorDeExpressoes(String expressao) {
 		root = null;
 		pilhaArray<String> pilhaOperadores = new pilhaArray<String>();
 		pilhaArray<Node<String>> pilhaArvores = new pilhaArray<Node<String>>();
 		String Operadores = "+-*/^~", valor;
 		for (int i = 0; i < expressao.length(); i++) {
-			if(expressao.charAt(i) == ' '){
+			if (expressao.charAt(i) == ' ') {
 				continue;
 			}
 			valor = "";
-			//caso parenteses abrindo
+			// caso parenteses abrindo
 			if (expressao.charAt(i) == '(') {
 				while (Operadores.indexOf(expressao.charAt(i)) == -1 && expressao.charAt(i) != (")").charAt(0)) {
-					if(expressao.charAt(i) == ' '){
+					if (expressao.charAt(i) == ' ') {
 						i++;
 						continue;
 					}
@@ -78,20 +84,20 @@ public class ArvoreExpressoes {
 				Node<String> nodeAux = new Node<String>();
 				nodeAux.setElemento(valor);
 				pilhaArvores.push(nodeAux);
-				if(expressao.charAt(i) == ')'){
+				if (expressao.charAt(i) == ')') {
 					continue;
 				}
 				pilhaOperadores.push(expressao.charAt(i) + "");
-			} 
-			//caso seja um operador
-			else if(Operadores.indexOf(expressao.charAt(i)) != -1){
+			}
+			// caso seja um operador
+			else if (Operadores.indexOf(expressao.charAt(i)) != -1) {
 				pilhaOperadores.push(expressao.charAt(i) + "");
 			}
-			//caso seja um valor
+			// caso seja um valor
 			else if (expressao.charAt(i) != ')') {
 				valor = "";
 				while (expressao.charAt(i) != ')') {
-					if(expressao.charAt(i) == ' '){
+					if (expressao.charAt(i) == ' ') {
 						i++;
 						continue;
 					}
@@ -103,8 +109,8 @@ public class ArvoreExpressoes {
 				nodeAux.setElemento(valor);
 				pilhaArvores.push(nodeAux);
 			}
-			//caso seja um parenteses fechando
-			else{
+			// caso seja um parenteses fechando
+			else {
 				Node<String> novoElemento = new Node<String>();
 				novoElemento.setElemento(pilhaOperadores.pop());
 				novoElemento.setRightNode(pilhaArvores.pop());
@@ -121,7 +127,8 @@ public class ArvoreExpressoes {
 			if (node.getLeftNode() == null && node.getRightNode() == null) {
 				return node.getElemento();
 			} else {
-				return "(" + auxExpressaoEmOrdem(node.getLeftNode()) + node.getElemento() + auxExpressaoEmOrdem(node.getRightNode()) + ")";
+				return "(" + auxExpressaoEmOrdem(node.getLeftNode()) + node.getElemento()
+						+ auxExpressaoEmOrdem(node.getRightNode()) + ")";
 			}
 		}
 		return null;
@@ -136,27 +143,29 @@ public class ArvoreExpressoes {
 			root.setLeftNode(novoElemento);
 		}
 	}
-	
-	private BigDecimal auxCalcularExpressao(Node<String> raiz, ListaArray<String> variaveis,ListaArray<BigDecimal> valoresCorrespondentes){
-		if(root != null){
-			if(raiz.getLeftNode() == null && raiz.getRightNode() == null){
+
+	private BigDecimal auxCalcularExpressao(Node<String> raiz, ListaArray<String> variaveis,
+			ListaArray<BigDecimal> valoresCorrespondentes) {
+		if (root != null) {
+			if (raiz.getLeftNode() == null && raiz.getRightNode() == null) {
 				String numeros = "0123456789";
-				if(numeros.indexOf(raiz.getElemento().charAt(0)) == -1){
-					if(variaveis.contem(raiz.getElemento().charAt(0)+"")){
-						return valoresCorrespondentes.get(variaveis.indexOf(raiz.getElemento().charAt(0)+""));
-					}else{
+				if (numeros.indexOf(raiz.getElemento().charAt(0)) == -1) {
+					if (variaveis.contem(raiz.getElemento().charAt(0) + "")) {
+						return valoresCorrespondentes.get(variaveis.indexOf(raiz.getElemento().charAt(0) + ""));
+					} else {
 						Scanner sc = new Scanner(System.in);
 						variaveis.adicionar(raiz.getElemento().charAt(0) + "");
 						System.out.println("informe o valor de " + raiz.getElemento().charAt(0));
 						BigDecimal tmp = new BigDecimal(sc.nextLine());
 						valoresCorrespondentes.adicionar(tmp);
+						sc.close();
 						return tmp;
 					}
 				}
 				return new BigDecimal(raiz.getElemento());
 			}
-			BigDecimal expressao1 = auxCalcularExpressao(raiz.getLeftNode(),variaveis,valoresCorrespondentes);
-			BigDecimal expressao2 = auxCalcularExpressao(raiz.getRightNode(),variaveis,valoresCorrespondentes);
+			BigDecimal expressao1 = auxCalcularExpressao(raiz.getLeftNode(), variaveis, valoresCorrespondentes);
+			BigDecimal expressao2 = auxCalcularExpressao(raiz.getRightNode(), variaveis, valoresCorrespondentes);
 			switch (raiz.getElemento()) {
 			case "*":
 				return expressao1.multiply(expressao2);
@@ -170,24 +179,24 @@ public class ArvoreExpressoes {
 				BigDecimal retorno = expressao1.pow(expressao2.intValue());
 				BigDecimal numerosAposVirgula = expressao2.subtract(BigDecimal.valueOf(expressao2.intValue()));
 				int valorRaiz = 1;
-				for(int i=0; i< numerosAposVirgula.toString().length()-2;i++){
-					valorRaiz = valorRaiz*10;
+				for (int i = 0; i < numerosAposVirgula.toString().length() - 2; i++) {
+					valorRaiz = valorRaiz * 10;
 					numerosAposVirgula = numerosAposVirgula.multiply(BigDecimal.TEN);
 				}
 				BigDecimal retornoDecimal = BigDecimal.ONE;
-				if(numerosAposVirgula.intValue()!=0){
-					retornoDecimal = auxCalcularExpressao(raiz.getLeftNode(),variaveis,valoresCorrespondentes);
+				if (numerosAposVirgula.intValue() != 0) {
+					retornoDecimal = auxCalcularExpressao(raiz.getLeftNode(), variaveis, valoresCorrespondentes);
 					retornoDecimal = retornoDecimal.pow(numerosAposVirgula.intValue());
 				}
-				retorno = retorno.multiply(raiz(retornoDecimal, valorRaiz, 40));
+				retorno = retorno.multiply(raiz(retornoDecimal, valorRaiz));
 				return retorno;
 			case "~":
-				return raiz(expressao1,expressao2.intValue(),40);
+				return raiz(expressao1, expressao2.intValue());
 			}
 		}
 		return BigDecimal.ZERO;
 	}
-	
+
 	private void auxInformaVariaveis(Node<String> raiz, ListaArray<String> variaveis,
 			ListaArray<String> valoresCorrespondentes) {
 		if (raiz != null) {
@@ -205,6 +214,7 @@ public class ArvoreExpressoes {
 						valoresCorrespondentes.adicionar(sc.nextLine());
 						raiz.setElemento(
 								valoresCorrespondentes.get(variaveis.indexOf(raiz.getElemento().charAt(0) + "")));
+						sc.close();
 					}
 				}
 			}
@@ -212,12 +222,31 @@ public class ArvoreExpressoes {
 		}
 
 	}
-	
-	private BigDecimal raiz(BigDecimal valor,int indice,int iteracoes){
+
+	/*
+	 * antigo private BigDecimal raiz(BigDecimal valor, int indice, int
+	 * iteracoes) { BigDecimal resultado = BigDecimal.ONE; for (int i = 0; i <
+	 * iteracoes; i++) { resultado =
+	 * resultado.subtract(resultado.pow(indice).subtract(valor)
+	 * .divide(resultado.pow(indice - 1).multiply(BigDecimal.valueOf(indice)),
+	 * mathContext)); } return resultado; }
+	 */
+	// novo
+	private BigDecimal raiz(BigDecimal valor, int indice) {
 		BigDecimal resultado = BigDecimal.ONE;
-		for(int i = 0; i < iteracoes; i++){
-			resultado = resultado.subtract(resultado.pow(indice).subtract(valor).divide(resultado.pow(indice-1).multiply(BigDecimal.valueOf(indice)),mathContext));
+		BigDecimal auxResultado = resultado;
+		BigDecimal k1 = (BigDecimal.ONE).divide(BigDecimal.valueOf(indice), mathContext);
+		int k2 = indice - 1;
+		while (true) {
+			resultado = k1.multiply(
+					resultado.multiply(BigDecimal.valueOf(k2)).add(valor.divide(resultado.pow(k2), mathContext)));
+			resultado = resultado.setScale(mathContext.getPrecision(), mathContext.getRoundingMode());
+			auxResultado = auxResultado.setScale(mathContext.getPrecision(), mathContext.getRoundingMode());
+			if (resultado.equals(auxResultado))
+				break;
+			auxResultado = resultado;
 		}
 		return resultado;
+
 	}
 }
--- src/testes/TesteArvoreExpressoes.java
+++ src/testes/TesteArvoreExpressoes.java
@@ -6,12 +6,14 @@ public class TesteArvoreExpressoes {
 	public static void main(String[] args) {
 		ArvoreExpressoes a = new ArvoreExpressoes();
 		a.armazeneExpressao("(2 ~ 1000)");
+		// a.armazeneExpressao("2~2");
 		/*
-		obs metodo de newton se demonstra impreciso com raizes como a acima, ou em 
-		casos de expoentes de mais de 3 casas decimais alem de apresentar lentidao
-		exagerada e necessidade de um alto numero de interações para alcancar a precisao 
-		da calculadora android, analisar outro algoritimo para funcionalidade
-		*/ 
+		 * obs metodo de newton se demonstra impreciso com raizes como a acima,
+		 * ou em casos de expoentes de mais de 3 casas decimais alem de
+		 * apresentar lentidao exagerada e necessidade de um alto numero de
+		 * interações para alcancar a precisao da calculadora android, analisar
+		 * outro algoritimo para funcionalidade
+		 */
 		System.out.println(a.expressaoEmOrdem());
 		System.out.println(a.calcularExpressao());
 	}
