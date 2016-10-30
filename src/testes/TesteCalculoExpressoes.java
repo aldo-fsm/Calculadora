5d6edd884cd1d8e8d0331836c1dd4c526fe94e98
Aryell Dias de Menezes <aryelld@gmail.com>
2016-10-30 14:28:14 -0300

    Update TesteCalculoExpressoes.java

--- src/testes/TesteCalculoExpressoes.java
+++ src/testes/TesteCalculoExpressoes.java
@@ -6,29 +6,32 @@ import estruturasDados.arvore.ArvoreExpressoes;
 
 public class TesteCalculoExpressoes {
 	public static void main(String[] args) {
-
 		ArvoreExpressoes calculadora = new ArvoreExpressoes();
-
-		System.out.println("\nTeste - Calculadora de Expressões");
-		System.out.println("digite exit para sair\n\n");
+		System.out.println("\nTeste - Calculadora de ExpressÃµes");
+		System.out.println("digite exit para sair");
+		System.out.println("digite acrescentar para acrescentar a uma expressao previamente incluida\n\n");
 		String input = "";
 		Scanner s = new Scanner(System.in);
-
 		while (true) {
-
-			System.out.printf(">>> ");
-			input = s.nextLine();
-			if (input.equals("exit")) {
-				break;
-			}
 			try {
-				calculadora.armazenarExpressao(input);
+				System.out.printf(">>> ");
+				input = s.nextLine();
+				if (input.equals("exit")) {
+					break;
+				} else if (input.equals("acrescentar")) {
+					System.out.println("digite a expressao a ser acrescentada sem parenteses");
+					System.out.printf(">>> ");
+					input = s.nextLine();
+					calculadora.acrescentaNaExpressao(input);
+				}else{
+					calculadora.armazenarExpressao(input);
+				}
 				calculadora.informarVariaveis();
 				System.out.println(calculadora.expressaoEmOrdem() + " = " + calculadora.calcularExpressao());
 			} catch (RuntimeException e) {
 				e.printStackTrace(System.out);
 			}
 		}
-
+		s.close();
 	}
 }
