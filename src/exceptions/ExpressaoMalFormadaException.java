13f5329b86c9f5de6a118b38cd7f85ff72a0ccfa
Aryell Dias de Menezes <aryelld@gmail.com>
2016-11-13 15:37:34 -0300

    Update ExpressaoMalFormadaException.java

--- src/exceptions/ExpressaoMalFormadaException.java
+++ src/exceptions/ExpressaoMalFormadaException.java
@@ -5,6 +5,6 @@ public class ExpressaoMalFormadaException extends RuntimeException{
 		super("Expressao mal formada!");
 	}
 	public ExpressaoMalFormadaException(String causa) {
-		super("Expressao mal formada:\n" + causa);
+		super("Expressao mal formada: " + causa);
 	}
 }
