abd188f9638eb44b2ce96d7a0f925131d8f4b89d
aldo <aldo.fsmonteiro@gmail.com>
2016-11-17 16:04:53 -0300

    serial version na exception

--- src/exceptions/ExpressaoMalFormadaException.java
+++ src/exceptions/ExpressaoMalFormadaException.java
@@ -1,9 +1,13 @@
 package exceptions;
 
-public class ExpressaoMalFormadaException extends RuntimeException{
+public class ExpressaoMalFormadaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
 	public ExpressaoMalFormadaException() {
 		super("Expressao mal formada!");
 	}
+
 	public ExpressaoMalFormadaException(String causa) {
 		super("Expressao mal formada: " + causa);
 	}
